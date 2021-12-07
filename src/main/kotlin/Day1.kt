26db0b52eb8d9b14d0c58f1f7fdd6e1ccea8d6db
Pierre Fourreau <fourreau.pierre@gmail.com>
2021-12-07 22:18:36 +0100

    Day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,8 +1,37 @@
 package main.kotlin
 
-class Day1 {
-}
+import java.io.File
 
 fun main() {
-    println("TODO")
+    val lines = File("src/main/input/day1.txt").readLines()
+    val expenses = lines.map(String::toInt)
+
+    partOne(expenses)
+    partTwo(expenses)
+}
+
+private fun partOne(expenses: List<Int>) {
+    expenses.forEach { expense1 ->
+        expenses.forEach { expense2 ->
+            if (expense1 + expense2 == 2020) {
+                println("Result part 1 " + expense1 * expense2)
+                return
+            }
+        }
+    }
+    println("No result found")
+}
+
+private fun partTwo(expenses: List<Int>) {
+    expenses.forEach { expense1 ->
+        expenses.forEach { expense2 ->
+            expenses.forEach { expense3 ->
+                if (expense1 + expense2 + expense3 == 2020) {
+                    println("Result part 1 " + expense1 * expense2 * expense3)
+                    return
+                }
+            }
+        }
+    }
+    println("No result found")
 }
